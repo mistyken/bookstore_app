09bc2d6c18e388141d301302e6214cbd618ea8ce
Kenneth Chen <mistyken@gmail.com>
2019-05-05 17:06:58 -0700

    look for book by isbn

--- app/book.py
+++ app/book.py
@@ -9,5 +9,9 @@ bp = Blueprint('book', __name__, url_prefix='/book')
 
 @bp.route('/')
 def index():
-    books = Book.objects
+    book_isbn = request.args.get('isbn')
+    if book_isbn is not None:
+        books = Book.objects(isbn=book_isbn)
+    else:
+        books = Book.objects
     return str([book.to_json() for book in books])
